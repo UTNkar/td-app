12a395dcfac1561c793c9cb17058e2dd60d9bb5a
Daniel Westberg <daniel.ar.westberg@gmail.com>
2021-05-11 19:26:22 +0200

    changed from Saxen to Click

--- src/static/texts/Mediaka.ts
+++ src/static/texts/Mediaka.ts
@@ -1,56 +1,56 @@
-{/*Change description of Mediaka here*/}
+{/*Change description of Mediaka here*/ }
 export const MediakaText = "Gröna, sköna och iklädda fräsigt färgglada ovvar, vilka kan det vara? Det är såklart Mediakå som ständigt befinner sig i närheten för att fånga de strålande ting som reccen upplever under mottagningens veckor. Mediakå lägger (nästan) all sin vakna, ovakna och halvvakna tid på att dokumentera allt från brunch till tältdans i bild och film, så att Reccen alltid kan vända sig till recce.utn.se när minnet svackar. Mediakå strålar utterskt mycket när de får umgås med Reccen, oavsett om det är fuldans i tältet, mys i Reccentralen eller om Reccen gör ett besök i det mustiga fotobåset utanför tältet. Strålningen strålar som starkast när Mediakå får bjuda reccen på en flott brunch på Uttgård där mumsig mat kommer serveras, bland annat utterbart uttmärkt crispiga våfflor. Hoppas vi ses där! OBS! Vet Reccen att om Reccen fastnat på bild eller film men inte tycker att det känns okej? Då ska Reccen höra av sig på Mediakås mailadress, mk@recce.utn.se. Ingen ska känna sig tvingad att vara med."
 
-{/*Change the program of a Mediakait here*/}
-{/*Correct for 2020*/}
-const ActionProgram ='X & K';
-const EffektenProgram ='STS & ES';
-const KonkavProgram ='M & W';
-const KonvexProgram ='DV & E/Ei';
-const MemoryProgram ='B & MT';
-const PlayProgram ='IT';
-const ProducentenProgram ='Q & I';
-const SaxenProgram ='F';
+{/*Change the program of a Mediakait here*/ }
+{/*Correct for 2020*/ }
+const ActionProgram = 'X & K';
+const EffektenProgram = 'STS & ES';
+const KonkavProgram = 'M & W';
+const KonvexProgram = 'DV & E/Ei';
+const MemoryProgram = 'B & MT';
+const PlayProgram = 'IT';
+const ProducentenProgram = 'Q & I';
+const ClickProgram = 'F';
 
 
-{/*Change the long program of a Mediakit here*/}
-{/*Correct for 2020*/}
-const ActionLongProgram ='Molekylär bioteknik & Kemiteknik';
-const EffektenLongProgram ='System i teknik och samhälle & Energisystem';
-const KonkavLongProgram ='Maskinteknik & Miljö- och vattenteknik';
-const KonvexLongProgram ='Datavetenska & Elektroteknik';
-const MemoryLongProgram ='Byggteknik & Medicinsk teknik';
-const PlayLongProgram ='Informationsteknologi';
-const ProducentenLongProgram ='Teknisk fysik med materialvetenskap & Industriell ekonomi';
-const SaxenLongProgram ='Teknisk fysik';
+{/*Change the long program of a Mediakit here*/ }
+{/*Correct for 2020*/ }
+const ActionLongProgram = 'Molekylär bioteknik & Kemiteknik';
+const EffektenLongProgram = 'System i teknik och samhälle & Energisystem';
+const KonkavLongProgram = 'Maskinteknik & Miljö- och vattenteknik';
+const KonvexLongProgram = 'Datavetenska & Elektroteknik';
+const MemoryLongProgram = 'Byggteknik & Medicinsk teknik';
+const PlayLongProgram = 'Informationsteknologi';
+const ProducentenLongProgram = 'Teknisk fysik med materialvetenskap & Industriell ekonomi';
+const ClickLongProgram = 'Teknisk fysik';
 
 
-{/*Change the text of a Mediakait here*/}
-const ActionText =  "Med en mulloändlig fantasi och mulloändligt många manus kommer Action att se till att Reccen får en oförglömlig tidsresa. Skulle Reccen sakna lite Action under mottagningen, är Action alltid redo för att hjälpa Reccen att drömma sig bort till en Actionfylld värd. Eller varför inte prova en Actionfylld reaktion på labb? Vare sig det handlar om mer eller mindre Action kommer Action att se till att Reccen får lagom med Action som räcker hela Reccens studietid och kanske till och med livet ut."
-const EffektenText ='En mullelurig feSTSjäl med skovligt sinne för EStetik, Effekten är alltid redo för nya roligheter! Då Reccen känner triSTSen gräva sig nära finns den gröna filuren där för att göra mottagningen effektfull igen. Effekten mår allra bESt då han bjuds på lakrits eller spontanspex, något Reccen mullvarligen bör uppmärksamma.';
+{/*Change the text of a Mediakait here*/ }
+const ActionText = "Med en mulloändlig fantasi och mulloändligt många manus kommer Action att se till att Reccen får en oförglömlig tidsresa. Skulle Reccen sakna lite Action under mottagningen, är Action alltid redo för att hjälpa Reccen att drömma sig bort till en Actionfylld värd. Eller varför inte prova en Actionfylld reaktion på labb? Vare sig det handlar om mer eller mindre Action kommer Action att se till att Reccen får lagom med Action som räcker hela Reccens studietid och kanske till och med livet ut."
+const EffektenText = 'En mullelurig feSTSjäl med skovligt sinne för EStetik, Effekten är alltid redo för nya roligheter! Då Reccen känner triSTSen gräva sig nära finns den gröna filuren där för att göra mottagningen effektfull igen. Effekten mår allra bESt då han bjuds på lakrits eller spontanspex, något Reccen mullvarligen bör uppmärksamma.';
 const KonkavText = "När ni är vuxna, skaffat villa, Volvo, och vovve, det är då ni vill minnas detta. Mottagningen. Men minnet kan bedra, det är då man behöver foton! För genom en kamera kan man inte bara fånga minnen, utan även tankar, idéer och känslor. Under er tid som Recce kommer mycket hända, både stora och små äventyr. Om inte Konkav är där och fångar allt, hur ska ni annars minnas Mottagningen?\nOm ni vill försäkra er om att ni fastnar på bild så kan ni bjuda mig på frysta vindruvor, sticklingar och en massa schyssta dansmoves."
 const KonvexText = "Konvex är redo med sin lins för att cEntrera allt ljus på Reccarna för att fånga deras mest mulleluriga poser och deras bästa stunder på bild inför framtidEn som de sedan kan kolla på när minnet börjar svika.\nOhm Reccen känner sig vilsen finns Konvex här för att göra Reccen meDVeten om allt roligt som händer och hjälper till med sina bästa aDVice om allt mellan himmel och underjord. Och sist men inte minst så är alla dåliga ordvitsar gulDVärda för att muntra upp en lins som Konvex.";
 const MemoryText = "Från Mullndal i GöteBorgstrakten är nu Memory på mullventyr i Uppsala för att mullkomna Reccarna hit! Under mottagningen går tiden fort när Reccen har roligt, så då är det mulleränt att Memory är här för att hjälpa Reccen hålla tiden ifall Reccen inte minns.\nTycker Reccen om våfflor lika mycket som Memory gör är Reccen äggstra mullkommen till den cyklotyMT minnesvärda MediaBrunchen! På kvällen sedan när Reccen har dansat i timmar och visat alla sina tidsvrängande moves kan Reccen dra med lite fotomullrande vänner till det mullvastiska fotoBåset för Bilder Reccen länge kommer minnas. Bjud gärna Memory på PimPim och skäMT.";
 const PlayText = "Ursprungen från den Västgôtska Schlätta ITererar denna MediakåIT fram på ITc (likt ett rekursivt anrop). Ta tillfället i akt och tryck på Play så får Reccen ta del av den mullventyrliga Mottagningen sedan tidernas begynnelse. Vill Reccen göra Play’s dag så bjud honom på mulltipla krambiljetter, dans eller annat valfrITt spexelexIT. Känner sig Reccen extra Playful kan Reccen alltid bjuda Play på ett mullvadiskt spex. För att Reccen ska få ny mullergi kan Reccen mullra sig till den mullvastiska Mediabrunchen.";
 const ProducentenText = "Inget sätter sig djupare än minnena som Producenten skapar tillsammans med sitt gröna och sköna Mediakå. Om Reccen skulle följa ett spår av smörtäckta popcorn-smulor så är chansen stor att Reccen kommer finna Prod vid slutet. Den mulleluriga Producenten har alltid något på gång och är som gladast när Prod lyckas samla de mullvastiskt klipska Recceklasserna från I & Q. Reccen kan vara säker på att Prod, förgylld med energin från Rekås oändliga Reccecium, alltid ger 100 Pro(du)¢ på dansgolvet. Har Reccen frågor om hur man lever i nuet som Prod? Kom då till Prod med hans oändliga IQ, så delar Prod gärna med sig av sin visdom.";
-const SaxenText = "SaXEn hallå! Mottagningens SAXikon har tagit sig hela vägen upp från Skåne för att hjälpa Reccen klösa genom dagen. SaXEn uppskattar allt från SAXofon solo till SAXrätters middag. Känner Reccen sig lite nere eller är i behov av energi är det bara att be SaXEn att visa sin Fabulöst SAXiga dans.\nPå sin fritid gillar SaXEn att spela Frisbee och spana på Fantastiskt Fagra mullFVadar ute på Ångströms exotiska stäpper.\nSaXEn kommer alltid finnas där för Reccen genom tid och rum, som en ståtlig klippa Reccen kan luta sig mot under livets stormar.";
+const ClickText = "Click hallå! Mottagningens SAXikon har tagit sig hela vägen upp från Skåne för att hjälpa Reccen klösa genom dagen. Click uppskattar allt från SAXofon solo till SAXrätters middag. Känner Reccen sig lite nere eller är i behov av energi är det bara att be Click att visa sin Fabulöst SAXiga dans.\nPå sin fritid gillar Click att spela Frisbee och spana på Fantastiskt Fagra mullFVadar ute på Ångströms exotiska stäpper.\nClick kommer alltid finnas där för Reccen genom tid och rum, som en ståtlig klippa Reccen kan luta sig mot under livets stormar.";
 
-{/*Change the quote of a Mediakait here*/}
+{/*Change the quote of a Mediakait here*/ }
 const ActionQuote = "'-This is impossible.'\n'-Only if you believe it is.'\n--Lewis Carroll, Alice in Wonderland";
-const EffektenQuote ='Mannen, myten, Effekten';
+const EffektenQuote = 'Mannen, myten, Effekten';
 const KonkavQuote = "Det är linsvätskan som gör bilderna bra";
 const KonvexQuote = "Vad kan man inte fixa i photoshop?";
 const MemoryQuote = "How many moles do you need to dig a hole? 6.022*10^23";
 const PlayQuote = "“Tackar tackar, tackar tackar”\n--David Sundin";
 const ProducentenQuote = "Det Producenten sätter i rullning stannar inte förrän Prod säger stop";
-const SaxenQuote = "SaXEn är vår hjälte!";
+const ClickQuote = "Click är vår hjälte!";
 
 
-export const Action = { name: "Andrea Garcia", program: ActionProgram, longProgram: ActionLongProgram, text: ActionText, quote:ActionQuote};
-export const Effekten = {name: "Vidar Lundgren",program: EffektenProgram, longProgram: EffektenLongProgram, text: EffektenText, quote:EffektenQuote};
-export const Konkav = {name: "Frida Adolfsson Lindahl",program: KonkavProgram, longProgram: KonkavLongProgram, text: KonkavText, quote:KonkavQuote};
-export const Konvex = {name: "Emma Kjellin",program: KonvexProgram, longProgram: KonvexLongProgram, text: KonvexText, quote:KonvexQuote};
-export const Memory = {name: "Tova Rane",program: MemoryProgram, longProgram: MemoryLongProgram, text: MemoryText, quote:MemoryQuote};
-export const Play = {name: "Carl Wallskog",program: PlayProgram, longProgram: PlayLongProgram, text: PlayText, quote:PlayQuote};
-export const Producenten = {name: "Gustav Halldén",program: ProducentenProgram, longProgram: ProducentenLongProgram, text: ProducentenText, quote:ProducentenQuote};
-export const Saxen = {name: "Nir Teyar",program: SaxenProgram, longProgram: SaxenLongProgram, text: SaxenText, quote:SaxenQuote};
+export const Action = { name: "Andrea Garcia", program: ActionProgram, longProgram: ActionLongProgram, text: ActionText, quote: ActionQuote };
+export const Effekten = { name: "Vidar Lundgren", program: EffektenProgram, longProgram: EffektenLongProgram, text: EffektenText, quote: EffektenQuote };
+export const Konkav = { name: "Frida Adolfsson Lindahl", program: KonkavProgram, longProgram: KonkavLongProgram, text: KonkavText, quote: KonkavQuote };
+export const Konvex = { name: "Emma Kjellin", program: KonvexProgram, longProgram: KonvexLongProgram, text: KonvexText, quote: KonvexQuote };
+export const Memory = { name: "Tova Rane", program: MemoryProgram, longProgram: MemoryLongProgram, text: MemoryText, quote: MemoryQuote };
+export const Play = { name: "Carl Wallskog", program: PlayProgram, longProgram: PlayLongProgram, text: PlayText, quote: PlayQuote };
+export const Producenten = { name: "Gustav Halldén", program: ProducentenProgram, longProgram: ProducentenLongProgram, text: ProducentenText, quote: ProducentenQuote };
+export const Click = { name: "Nir Teyar", program: ClickProgram, longProgram: ClickLongProgram, text: ClickText, quote: ClickQuote };

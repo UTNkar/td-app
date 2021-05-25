adfe51fd0b45bee5f1a84296369b6f32fa6efa12
Daniel Westberg <daniel.ar.westberg@gmail.com>
2021-05-25 18:05:05 +0200

    updated reccecentralen info text

--- src/screens/Reccecentralen.tsx
+++ src/screens/Reccecentralen.tsx
@@ -3,13 +3,13 @@ import { SafeAreaView, Dimensions, TouchableOpacity, Image, View, StyleSheet, Sc
 import TDHeader from '../components/TDHeader';
 
 const RecceCentralenHeader = () => (<View style={{ marginBottom: 50, flex: 1, width: '95%', alignSelf: 'center' }}>
-                                 <Text style={style.title}>Reccentralen</Text>
-                             </View >);
+    <Text style={style.title}>Reccentralen</Text>
+</View >);
 
 const Flags = () => (<Image source={require('../static/images/info/flaggor.png')}
-                            style={{ width: '100%', alignSelf: 'center', position: 'absolute', top: 10 }}
-                            resizeMode='contain'
-                     />)
+    style={{ width: '100%', alignSelf: 'center', position: 'absolute', top: 10 }}
+    resizeMode='contain'
+/>)
 
 
 const WIDTH = Dimensions.get('window').width;
@@ -17,13 +17,13 @@ const WIDTH = Dimensions.get('window').width;
 const RecceCentralen = ({ navigation }) => {
     return (
         <SafeAreaView style={style.safeContainer}>
-                <TDHeader />
+            <TDHeader />
             <ScrollView style={{ flex: 1, backgroundColor: '#342A3B' }}>
                 <View style={{ marginHorizontal: 10 }}>
                     <RecceCentralenHeader />
                     <Flags />
-                    <Text style={{...style.infoText, width: '95%'}}>
-                        Om Reccen behöver gömma sig undan mullväder, känner sig hungrig eller inte känner sig så mullerän finns den tidlöst bra Reccentralen som finns till för endast Reccen.{'\n\n'}
+                    <Text style={{ ...style.infoText, width: '95%' }}>
+                        Om Reccen behöver gömma sig undan paddastrofväder, känner sig hungrig eller inte känner sig så hoppedundrig finns den tidlöst bra Reccentralen som finns till för endast Reccen. Reccen kan här införskaffa sig tidernas Spännkort, ett Partykit eller biljetter till roliga kvällar såsom Klassfest.{'\n\n'}
                         Reccen kan här införskaffa sig tidernas <Text style={{ color: '#FFB01C', fontWeight: 'bold' }}>Spännkort</Text>, ett <Text style={{ color: '#FF71FF', fontWeight: 'bold' }}>Partykit</Text> eller <Text style={{ color: '#15B700', fontWeight: 'bold' }}>biljetter</Text> till roliga kvällar så som Klassfest och Finalgasque.
                     </Text>
                     <TouchableOpacity
@@ -34,17 +34,17 @@ const RecceCentralen = ({ navigation }) => {
                     </TouchableOpacity>
                 </View>
                 <Image source={require('../static/images/info/Reccentralen_FULL.png')}
-                       style={{ width: WIDTH, height: WIDTH * 0.69, marginBottom: 40 }} />
+                    style={{ width: WIDTH, height: WIDTH * 0.69, marginBottom: 40 }} />
                 <View style={{ paddingHorizontal: 10, marginBottom: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
                     <Text style={{ ...style.infoText, width: '70%' }}>
-                        Om Reccen har mullkrastinerat och inte fixat sina matlådor kan en såklart köpa lite lättare mulltimagisk <Text style={{ color: '#FF71FF', fontWeight: 'bold' }}>förtäring</Text>. Det erbjuds frysmat, läsk, glass och fika.{'\n'}
+                        Om Reccen har grodkrastinerat och inte fixat sina matlådor kan man såklart köpa lite lättare pluttiskuttig <Text style={{ color: '#FF71FF', fontWeight: 'bold' }}>förtäring</Text>. Det erbjuds frysmat, läsk, glass och fika. Reccen får inte heller missa den multidimensionella bra <Text style={{ color: '#FFB01C', fontWeight: 'bold' }}>uteplatesen</Text> där Reccen kan hämta andan eller kväka med sina nya kompisar. För denna <Text style={{ color: '#FFB01C', fontWeight: 'bold' }}>uteplats</Text> är en skyddad dimension och Reccen är skyddad från universums endimensionella strängar.{'\n'}
                     </Text>
                     <Image source={require('../static/images/info/glass.png')} style={{ width: '20%', height: '135%', marginRight: 15, alignSelf: 'center', resizeMode: 'contain' }} />
                 </View>
                 <View style={{ marginHorizontal: 10, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
                     <Image source={require('../static/images/info/spelkontroll.png')} style={{ width: 100, alignSelf: 'center', height: '100%', marginBottom: 30, resizeMode: 'contain' }} />
                     <Text style={{ ...style.infoText, width: '75%', textAlign: 'right', marginRight: 15 }}>
-                        Reccen får inte heller missa den underjordligt bra <Text style={{ color: '#FFB01C', fontWeight: 'bold' }}>Uteplatsen</Text> där Reccen kan hämta andan eller utmana sina nya kompisar i mulleräna <Text style={{ color: '#15B700', fontWeight: 'bold' }}>spel</Text>. För på denna uteplats står tiden still och oändlighetens långa arm kan inte komma åt Reccen.{'\n\n'}
+                        Är Reccen i disträ är det bara att utbrista ”Hjälp mig <Text style={{ color: '#FF71FF', fontWeight: 'bold' }}>Fadderkå</Text> eller <Text style={{ color: '#15B700', fontWeight: 'bold' }}>Mediakå</Text>, ni är mitt enda hopp!” så kommer <Text style={{ color: '#FF71FF', fontWeight: 'bold' }}>Fadderkå</Text> och <Text style={{ color: '#15B700', fontWeight: 'bold' }}>Mediakå</Text> på studs till undsättning då det alltid finns någon av dem på plats i Reccentralen.{'\n\n'}
                     </Text>
                 </View>
             </ScrollView >
